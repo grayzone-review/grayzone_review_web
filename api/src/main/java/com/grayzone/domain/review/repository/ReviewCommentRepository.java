700493ed9e2d8ffa40d0b9fc29fc88a00c76ce41
lxodud <xodud1998223@naver.com>
2025-05-23 23:24:52 +0900

    [#9] feat: ReviewCommentRepository 구현

---
+++ api/src/main/java/com/grayzone/domain/review/repository/ReviewCommentRepository.java
@@ -0,0 +1,7 @@
+package com.grayzone.domain.review.repository;
+
+import com.grayzone.domain.review.entity.ReviewComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Long> {
+}
