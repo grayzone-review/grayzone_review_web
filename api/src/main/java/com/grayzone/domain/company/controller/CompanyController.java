fdd7989bf2b4c7902ce6ce3520a3e502e15105ae
lxodud <xodud1998223@naver.com>
2025-05-26 09:30:26 +0900

    [#9] refactor: CompanyReviewController 수정

--- api/src/main/java/com/grayzone/domain/company/controller/CompanyController.java
+++ api/src/main/java/com/grayzone/domain/company/controller/CompanyController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/companies")
+@RequiredArgsConstructor
 public class CompanyController {
 
   private final CompanyService companyService;
