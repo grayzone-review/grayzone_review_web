065aeaea503d9f863295ff16457ac3627e74d563
lxodud <xodud1998223@naver.com>
2025-05-29 14:50:51 +0900

    [#11] feat: CreateReplyResponseDto 구현

---
+++ api/src/main/java/com/grayzone/domain/review/dto/response/CreateReplyResponseDto.java
@@ -0,0 +1,28 @@
+package com.grayzone.domain.review.dto.response;
+
+import com.grayzone.domain.review.entity.ReviewComment;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class CreateReplyResponseDto {
+  private Long id;
+  private String comment;
+  private String authorName;
+  private LocalDateTime createdAt;
+  private boolean isSecret;
+  private boolean isVisible;
+
+  public static CreateReplyResponseDto from(ReviewComment comment, Long viewerId) {
+    return CreateReplyResponseDto.builder()
+      .id(comment.getId())
+      .comment(comment.getComment())
+      .authorName(comment.getAuthorName())
+      .isSecret(comment.isSecret())
+      .isVisible(true)
+      .build();
+  }
+}
