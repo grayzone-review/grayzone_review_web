5ac528cef4244030656b0b5bfd6f85ae7a80a82a
lxodud <xodud1998223@naver.com>
2025-06-02 15:09:14 +0900

    [#13] feat: User 객체 UserDetails implement

--- api/src/main/java/com/grayzone/domain/user/entity/User.java
+++ api/src/main/java/com/grayzone/domain/user/entity/User.java
@@ -2,15 +2,30 @@ package com.grayzone.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String username;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return "";
+  }
 }
