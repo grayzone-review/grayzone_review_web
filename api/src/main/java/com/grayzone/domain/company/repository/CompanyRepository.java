3593c3252d2903139e404551f5d393d6c8acbd49
lxodud <xodud1998223@naver.com>
2025-06-06 22:29:08 +0900

    [#17] feat: findByKeywordOrderByDistance 메서드 구현

--- api/src/main/java/com/grayzone/domain/company/repository/CompanyRepository.java
+++ api/src/main/java/com/grayzone/domain/company/repository/CompanyRepository.java
@@ -1,8 +1,29 @@
 package com.grayzone.domain.company.repository;
 
 import com.grayzone.domain.company.entity.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+  @Query(value = """
+        SELECT *,
+            (6371 * acos(
+                cos(radians(:latitude)) * cos(radians(c.latitude)) *
+                cos(radians(c.longitude) - radians(:longitude)) +
+                sin(radians(:latitude)) * sin(radians(c.latitude))
+            )) AS distance
+        FROM companies c
+        WHERE c.business_name LIKE %:keyword%
+        ORDER BY distance ASC
+    """, nativeQuery = true)
+  Page<Company> findByKeywordOrderByDistance(
+    @Param("keyword") String keyword,
+    @Param("latitude") Double latitude,
+    @Param("longitude") Double longitude,
+    Pageable pageable
+  );
 }
