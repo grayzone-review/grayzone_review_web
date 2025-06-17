ffc0ed2081fe412113e1a1e35ebae47b6bac0225
lxodud <xodud1998223@naver.com>
2025-06-17 18:24:09 +0900

    [#22] feat: GeminiRequest 구현

---
+++ api/src/main/java/com/grayzone/global/gemini/GeminiRequest.java
@@ -0,0 +1,32 @@
+package com.grayzone.global.gemini;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class GeminiRequest {
+  private List<Content> contents;
+
+  public GeminiRequest(List<Content> contents) {
+    this.contents = contents;
+  }
+
+  @Getter
+  public static class Content {
+    private List<Part> parts;
+
+    public Content(List<Part> parts) {
+      this.parts = parts;
+    }
+  }
+
+  @Getter
+  public static class Part {
+    private String text;
+
+    public Part(String text) {
+      this.text = text;
+    }
+  }
+}
