6e00d4d8f9ade3d76af87da8ec5c868269d18c5f
lxodud <xodud1998223@naver.com>
2025-06-27 17:19:27 +0900

    [#28] feat: LegalDistrictRepository 구현

---
+++ api/src/main/java/com/grayzone/domain/legaldistrict/repository/LegalDistrictRepository.java
@@ -0,0 +1,8 @@
+package com.grayzone.domain.legaldistrict.repository;
+
+import com.grayzone.domain.legaldistrict.entity.LegalDistrict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LegalDistrictRepository extends JpaRepository<LegalDistrict, Long> {
+
+}
