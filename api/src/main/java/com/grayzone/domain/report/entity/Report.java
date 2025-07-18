cf0a3e722643ca3d65d7ad345263db816a3920bd
lxodud <xodud1998223@naver.com>
2025-07-18 17:45:27 +0900

    [#45] feat: 신고하기 테이블 구현

---
+++ api/src/main/java/com/grayzone/domain/report/entity/Report.java
@@ -0,0 +1,27 @@
+package com.grayzone.domain.report.entity;
+
+import com.grayzone.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reports")
+@NoArgsConstructor
+public class Report extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String reporterName;
+
+  @Column(nullable = false)
+  private String targetName;
+
+  @Column(nullable = false)
+  private String reportType;
+
+  @Column(nullable = false)
+  private String description;
+}
