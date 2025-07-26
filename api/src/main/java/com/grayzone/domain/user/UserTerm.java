12e82b324d6fb5fb1e53f414ce055e70e0ee406b
lxodud <xodud1998223@naver.com>
2025-07-26 19:41:49 +0900

    [#61] feat: 이용 약관 디테일 페이지 url 추가

--- api/src/main/java/com/grayzone/domain/user/UserTerm.java
+++ api/src/main/java/com/grayzone/domain/user/UserTerm.java
@@ -1,15 +1,32 @@
 package com.grayzone.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.grayzone.global.exception.UpError;
+import com.grayzone.global.exception.UpException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum UserTerm {
-  SERVICE("serviceUse", "서비스 이용 약관", true, ""),
-  PRIVACY("privacy", "개인정보 수집 및 이용 동의", true, ""),
-  LOCATION("location", "위치기반 서비스 동의", true, "");
+  SERVICE(
+    "serviceUse",
+    "서비스 이용 약관",
+    true,
+    "https://translucent-fall-6f6.notion.site/23c10746acf98094aa7ee08bf627d7f8?source=copy_link"
+  ),
+  PRIVACY(
+    "privacy",
+    "개인정보 수집 및 이용 동의",
+    true,
+    "https://translucent-fall-6f6.notion.site/23c10746acf980248e6ef9e7665dd3d3?source=copy_link"
+  ),
+  LOCATION(
+    "location",
+    "위치기반 서비스 동의",
+    true,
+    "https://translucent-fall-6f6.notion.site/23c10746acf9804fa5cec48c5f26b371?source=copy_link"
+  );
 
   private final String code;
   private final String title;
@@ -23,6 +40,6 @@ public enum UserTerm {
         return term;
       }
     }
-    throw new IllegalArgumentException("동의 항목을 확인해주세요: " + code);
+    throw new UpException(UpError.INVALID_REQUEST);
   }
 }
