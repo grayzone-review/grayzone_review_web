6857fcd9dfc2510a2051d567dc110b1359d276c7
lxodud <xodud1998223@naver.com>
2025-07-26 16:11:08 +0900

    [#55] feat: AppleRevokeService 구현

--- api/src/main/java/com/grayzone/global/oauth/apple/AppleRevokeService.java
+++ api/src/main/java/com/grayzone/global/oauth/apple/AppleRevokeService.java
@@ -1,8 +1,9 @@
-package com.grayzone.global.oauth.kakao;
+package com.grayzone.global.oauth.apple;
 
 import com.grayzone.domain.user.entity.User;
 import com.grayzone.global.oauth.OAuthProvider;
 import com.grayzone.global.oauth.OAuthRevokeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -11,32 +12,37 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Component
-public class KakaoRevokeService implements OAuthRevokeService {
+@RequiredArgsConstructor
+public class AppleRevokeService implements OAuthRevokeService {
+
   private final RestClient restClient = RestClient.create();
+  private final AppleUtils appleUtils;
 
-  @Value("${kakao.app-admin-key}")
-  private String kakaoAdminKey;
+  @Value("${apple.revoke-token-uri}")
+  private String appleRevokeTokenUri;
 
-  @Value("${kakao.revoke-uri}")
-  private String kakaoRevokeUri;
+  @Value("${apple.client-id}")
+  private String appleClientId;
 
   @Override
   public boolean support(OAuthProvider provider) {
-    return provider == OAuthProvider.KAKAO;
+    return provider == OAuthProvider.APPLE;
   }
 
   @Override
   public void revoke(User user) {
-    String oAuthId = user.getOAuthId();
+    String clientSecret = appleUtils.createClientSecret();
+    String refreshToken = user.getOAuthRefreshToken();
 
     MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-    body.add("target_id_type", "user_id");
-    body.add("target_id", oAuthId);
+    body.add("client_id", appleClientId);
+    body.add("client_secret", clientSecret);
+    body.add("token", refreshToken);
+    body.add("token_type_hint", "refresh_token");
 
     restClient.post()
-      .uri(kakaoRevokeUri)
+      .uri(appleRevokeTokenUri)
       .header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-      .header("Authorization", kakaoAdminKey)
       .body(body)
       .retrieve()
       .body(String.class);
